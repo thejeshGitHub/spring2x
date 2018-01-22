b7940548e825a7103034da0b4228821a51dacb16
tejesh.asara@gmail.com <tejesh.asara@gmail.com>
2018-01-22 11:54:53 +0530

    Changed sysout with v1 version

--- src/main/java/com/practice/springx/TempDemo.java
+++ src/main/java/com/practice/springx/TempDemo.java
@@ -4,7 +4,7 @@ public class TempDemo {
 
 	public static void main(String[] args) {
 	
-		System.out.println("From TempDemo");
+		System.out.println("From TempDemo v1");
 	}
 
 }
--- src/main/java/com/springx/practice/springxdemo/different/Spring2xDemo.java
+++ src/main/java/com/springx/practice/springxdemo/different/Spring2xDemo.java
@@ -2,6 +2,6 @@ package com.springx.practice.springxdemo.different;
 
 public class Spring2xDemo {
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+		System.out.println("Welcome v1");
 	}
 }
